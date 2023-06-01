4879dd067d11b9ee94b1c0e02d47d0658d4ca10c
antonskrebkov <anton.skrebkov777@gmail.com>
2023-06-01 13:31:22 +0300

    API changed for gh-pages deployment

--- src/services/ExchangeService.ts
+++ src/services/ExchangeService.ts
@@ -1,37 +1,43 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
-import IExchangeItem from '../interfaces/IExchangeItem';
+import IExchangeItem from "../interfaces/IExchangeItem";
 
 export const exchangeAPI = createApi({
-  reducerPath: 'exchangeAPI',
+  reducerPath: "exchangeAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3002'
+    // baseUrl: 'http://localhost:3002'
+    baseUrl: "https://64564f9d5f9a4f236140736d.mockapi.io",
   }),
-  tagTypes: ['Exchange'],
+  tagTypes: ["Exchange"],
   endpoints: (build) => ({
     getCurrencies: build.query({
-      query: () => '/currencies',
-      providesTags: result => ['Exchange']
+      query: () => "/currencies",
+      providesTags: (result) => ["Exchange"],
     }),
     getExchangeHistory: build.query<IExchangeItem[], string>({
-      query: () => '/exchange',
-      providesTags: result => ['Exchange']
+      query: () => "/exchange",
+      providesTags: (result) => ["Exchange"],
     }),
     addExchangeItem: build.mutation({
       query: (exchangeItem: IExchangeItem) => ({
-        url: '/exchange',
-        method: 'POST',
-        body: exchangeItem
+        url: "/exchange",
+        method: "POST",
+        body: exchangeItem,
       }),
-      invalidatesTags: ['Exchange']
+      invalidatesTags: ["Exchange"],
     }),
     deleteExchangeItem: build.mutation({
       query: (exchangeItem: IExchangeItem) => ({
         url: `/exchange/${exchangeItem.id}`,
-        method: 'DELETE'
+        method: "DELETE",
       }),
-      invalidatesTags: ['Exchange']
+      invalidatesTags: ["Exchange"],
     }),
-  })
-})
+  }),
+});
 
-export const {useGetExchangeHistoryQuery, useAddExchangeItemMutation, useDeleteExchangeItemMutation, useGetCurrenciesQuery} = exchangeAPI;
+export const {
+  useGetExchangeHistoryQuery,
+  useAddExchangeItemMutation,
+  useDeleteExchangeItemMutation,
+  useGetCurrenciesQuery,
+} = exchangeAPI;
